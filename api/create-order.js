2ee8af6d4c5e4616a28cdc4012741dc44682eb28
alauddinhf788-git <alauddinhf788@gmail.com>
2025-11-17 00:38:41 +0600

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+
+res.setHeader("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN);
+res.setHeader("Access-Control-Allow-Methods", "POST");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
