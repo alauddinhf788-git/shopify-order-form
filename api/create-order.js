24de89f806475337048805bf091813fc6bead0cc
alauddinhf788-git <alauddinhf788@gmail.com>
2025-11-17 05:58:06 +0600

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
         customer: {
           first_name: name,
           phone: phone,
-        
+          address: address,
         },
 
         line_items: [
